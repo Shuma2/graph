5f25b40b8bae4b996de7501677b63797b42d88d9
Shuma2 <shumka2004@gmail.com>
2016-01-11 19:36:03 +0200

    starting by project

---
+++ inc/db.inc.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shuma
+ * Date: 11.01.2016
+ * Time: 19:12
+ */
---
+++ php/nav.html.php
@@ -0,0 +1,11 @@
+<div class="nav">
+    <div class="container">
+        <div id='cssmenu'>
+            <ul>
+                <li class='active'><a href='#'><span>Graph</span></a></li>
+                <li><a href='#'><span>Notes</span></a></li>
+                <li c      lass='last'><a href='#'><span>Archive</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
