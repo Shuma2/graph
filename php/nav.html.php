fd85931d1d2a1aedb23037b382fbc49dd35ecb73
Arik <shumka2004@gmail.com>
2016-01-25 16:50:09 +0200

    "Graph" in main menu returns to main page

--- php/nav.html.php
+++ php/nav.html.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div id='cssmenu'>
             <ul>
-                <li class='active'><a href='#'><span>Graph</span></a></li>
+                <li class='active'><a href='..'><span>Graph</span></a></li>
                 <li><a href='#'><span>Notes</span></a></li>
                 <li class='last'><a href='#'><span>Archive</span></a></li>
             </ul>
