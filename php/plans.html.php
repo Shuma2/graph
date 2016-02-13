0789a818921e964327ac1b8ccb3a4c4c8f87065e
Shuma2 <shumka2004@gmail.com>
2016-02-13 19:07:28 +0200

    new page "Notes"

--- php/plans.html.php
+++ php/plans.html.php
@@ -2,9 +2,21 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Title</title>
+    <title>Error</title>
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/menu.css" rel="stylesheet">
 </head>
 <body>
-
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/nav.html.php'; ?>
+<div class="container">
+    <div class="jumbotron" align="center">
+        <h1>Page is in developing</h1>
+    </div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/footer.html.php'; ?>
 </body>
 </html>
--- php/plans.php
+++ php/plans.php
@@ -1,7 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Shuma
- * Date: 13.02.2016
- * Time: 18:54
- */
+
+
+include 'plans.html.php';
