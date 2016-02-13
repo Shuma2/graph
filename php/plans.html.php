450cacd20b4d59fc2511b075b60fd7f14f37bfd6
Shuma2 <shumka2004@gmail.com>
2016-02-13 20:48:53 +0200

    small repair

--- php/plans.html.php
+++ php/plans.html.php
@@ -2,19 +2,30 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Error</title>
+    <title>Plans</title>
     <!-- Bootstrap -->
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
 
-    <link href="/css/main.css" rel="stylesheet">
+    <!--  My  -->
     <link href="/css/menu.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/datepicker.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.png" type="image/png">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="http://d3js.org/d3.v3.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/php/nav.html.php'; ?>
 <div class="container">
     <div class="jumbotron" align="center">
-        <h1>Page is in developing</h1>
+        <h1>Under construction</h1>
     </div>
 </div>
 <?php include $_SERVER['DOCUMENT_ROOT'] . '/php/footer.html.php'; ?>
