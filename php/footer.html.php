14b5e8235242f3032717073aa17357dc5fa33bce
Shuma2 <shumka2004@gmail.com>
2016-02-13 10:54:28 +0200

    version 1.0.3

--- php/footer.html.php
+++ php/footer.html.php
@@ -1,7 +1,7 @@
 <div class="footer">
     <div class="container">
         <div class="panel panel-footer">
-            <p>Graph 1.0.2 &copy; Shum 2015 &ndash; <?php echo date('Y');?></p>
+            <p>Graph 1.0.3 &copy; Shum 2015 &ndash; <?php echo date('Y');?></p>
         </div>
     </div>
 </div>
