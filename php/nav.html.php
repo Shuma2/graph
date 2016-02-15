ad46db871eb186df13842a11d72a660998d04518
Shuma2 <shumka2004@gmail.com>
2016-02-15 20:10:53 +0200

    changed nav bar

--- php/nav.html.php
+++ php/nav.html.php
@@ -1,11 +1,18 @@
-<div class="nav">
-    <div class="container">
-        <div id='cssmenu'>
-            <ul>
-                <li><a href='/index.php'><span>Graph</span></a></li>
+<div class="container">
+    <nav class="navbar navbar-default">
+        <div class="container-fluid">
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/index.php">Graph</a>
+            </div>
+            <ul class="nav navbar-nav">
+                <li><a href='/index.php'><span>Affairs</span></a></li>
                 <li><a href='/php/plans.php'><span>Plans</span></a></li>
                 <li><a href='/php/archive.php'><span>Archive</span></a></li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+                <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            </ul>
         </div>
-    </div>
+    </nav>
 </div>
