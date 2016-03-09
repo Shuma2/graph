4eb6b3aab527a385b7701c5bac1111a15a14b740
Shuma2 <shumka2004@gmail.com>
2016-03-09 20:42:41 +0200

    Version 1.0.6

---
+++ php/views/login.html.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Plans</title>
+    <!-- Bootstrap -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="http://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css">
+
+    <!--  My  -->
+    <link href="/css/menu.css" rel="stylesheet">
+    <link href="/css/main.css" rel="stylesheet">
+    <link href="/css/datepicker.css" rel="stylesheet">
+    <link rel="shortcut icon" href="/favicon.png" type="image/png">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <script src="http://d3js.org/d3.v3.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/views/nav.html.php'; ?>
+<div class="container">
+    <div class="jumbotron" align="center">
+        <h1>Under construction</h1>
+        <div class="body-content" align="left">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2>Features</h2>
+                    <p class="lead">• Plans for departments with roles</p>
+                    <p class="lead">• Plans for long time</p>
+                    <p class="lead">• Plans for every day</p>
+                    <p class="lead">• SMS/Email notifications if "every day" plans aren't done</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include $_SERVER['DOCUMENT_ROOT'] . '/php/views/footer.html.php'; ?>
+</body>
+</html>
