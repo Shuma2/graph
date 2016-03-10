868242075e6a45f43d31d66a010c50bd57970aad
Shuma2 <shumka2004@gmail.com>
2016-03-10 21:28:07 +0200

    started OOP work

--- php/controllers/plans.php
+++ php/controllers/plans.php
@@ -1,4 +1,24 @@
 <?php
-$day = 6;
+class Plans
+{
+    protected $main;
+    protected $timeEveryDay;
+    protected $timeLifeTime;
+
+    public function setMain($main)
+    {
+        $this->main = $main;
+    }
+
+    public function setTimeEveryDay($timeEveryDay)
+    {
+        $this->timeEveryDay = $timeEveryDay;
+    }
+
+    public function setTimeLifeTime($timeLifeTime)
+    {
+        $this->timeLifeTime = $timeLifeTime;
+    }
+}
 
 include $_SERVER['DOCUMENT_ROOT'] . '/php/views/plans.html.php';
