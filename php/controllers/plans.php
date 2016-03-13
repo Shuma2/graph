2e26b9212f49f77d4793a204f43e8ca5bcc42b06
Shuma2 <shumka2004@gmail.com>
2016-03-13 19:15:08 +0200

    daily plans

--- php/controllers/plans.php
+++ php/controllers/plans.php
@@ -19,6 +19,26 @@ class Plans
     {
         $this->timeLifeTime = $timeLifeTime;
     }
+
+    public function getMain()
+    {
+        return $this->main;
+    }
+
+    public function getTimeEveryDay()
+    {
+        return $this->timeEveryDay;
+    }
+
+    public function getTimeLifeTime()
+    {
+        return $this->timeLifeTime;
+    }
+
+    public function setDailyWork($pdo, $insertMain = array())
+    {
+
+    }
 }
 
 include $_SERVER['DOCUMENT_ROOT'] . '/php/views/plans.html.php';
